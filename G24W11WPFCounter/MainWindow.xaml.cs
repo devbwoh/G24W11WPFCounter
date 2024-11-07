be670d2d362b1737381d1802295a6d0973e77cfd
BWOH <bwoh@outlook.kr>
2024-11-07 09:31:18 +0900

    초기 커밋.

---
+++ G24W11WPFCounter/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace G24W11WPFCounter {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        protected int Count = 0;
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void OnAdd(object sender, RoutedEventArgs e) {
+            //BtnAdd.Content = "눌렸습니다";
+            //TxtCount.Text = "눌렸습니다";
+            TxtCount.Text = $"{++Count}";
+        }
+
+        private void OnSub(object sender, RoutedEventArgs e) {
+            if (Count > 0) {
+                TxtCount.Text = $"{--Count}";
+            }
+        }
+    }
+}
